a9aa8861b0281ef27f94ed52c82fbe2d55acbdc5
enesagu <97469455+enesagu@users.noreply.github.com>
2024-02-21 16:27:41 +0300

    Added GET endpoint for fetching contacts
    Implemented a GET endpoint in main.py to retrieve contacts data from the database and return it as JSON format. Converted SQLAlchemy query result to a list of JSON objects for compatibility with the jsonify function.

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://flask_python_app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_python_app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
 db = SQLAlchemy(app)
