433c0b0d7328ff0186971d48f9cea0f161d8b7b4
enesagu <97469455+enesagu@users.noreply.github.com>
2024-02-28 14:16:44 +0300

    Final
    Final

--- backend/main.py
+++ backend/main.py
@@ -11,27 +11,25 @@ def get_contacts():
 
 
 @app.route("/create_contact", methods=["POST"])
-def creat_contact():
+def create_contact():
     first_name = request.json.get("firstName")
     last_name = request.json.get("lastName")
     email = request.json.get("email")
 
-    
     if not first_name or not last_name or not email:
-        return jsonify({"message":"You must include a first name, last name and email"}),
-        400,
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}),
+            400,
+        )
 
-    new_contact = Contact(first_name=first_name,last_name=last_name,email=email)
-
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
         db.session.add(new_contact)
         db.session.commit()
     except Exception as e:
-        return jsonify({"message":str(e)}),400
-    
+        return jsonify({"message": str(e)}), 400
 
-    return jsonify({"message":"User created!"}), 201
-
+    return jsonify({"message": "User created!"}), 201
 
 
 @app.route("/update_contact/<int:user_id>", methods=["PATCH"])
@@ -39,8 +37,7 @@ def update_contact(user_id):
     contact = Contact.query.get(user_id)
 
     if not contact:
-        return jsonify({"message":"User not found"}), 404
-
+        return jsonify({"message": "User not found"}), 404
 
     data = request.json
     contact.first_name = data.get("firstName", contact.first_name)
@@ -49,31 +46,24 @@ def update_contact(user_id):
 
     db.session.commit()
 
-    return jsonify({"message":"User updated"}),200
+    return jsonify({"message": "Usr updated."}), 200
 
 
-@app.route("/delete/<int:user_id>", methods=["DELETE"])
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
 
     if not contact:
-        return jsonify({"message":"User not found"}), 404
-
+        return jsonify({"message": "User not found"}), 404
 
     db.session.delete(contact)
     db.session.commit()
 
-    return jsonify({"message":"User deleted"})
+    return jsonify({"message": "User deleted!"}), 200
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-
-
     app.run(debug=True)
-
